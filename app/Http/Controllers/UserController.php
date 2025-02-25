171bcbcd8bf5bdaa5937f75a08874ceebd4e934a
SuryaRf <ayruzrahmad@gmail.com>
2025-02-25 21:22:25 +0700

    update user controller, view and model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,12 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function show($id, $name)
+
+    public function index()
     {
-        return view('user.profile', compact('id', 'name'));
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'name' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
+
+// // tambah data user dengan Eloquent Model
+// $data = [
+//     'username' => 'customer-1',
+//     'name' => 'Pelanggan',
+//     'password' => Hash::make('12345'),
+//     'level_id' => 4
+// ];
+
+// UserModel::insert($data); // tambahkan data ke tabel m_user
+
+// // coba akses model UserModel
+// $user = UserModel::all(); // ambil semua data dari tabel m_user
+// return view('user', ['data' => $user]);
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
@@ -13,13 +14,15 @@ Route::get('/', function (){
     return view('welcome');
 });
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-});
 
 Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
 Route::get('/sales', [SalesController::class, 'index']);
+// Route::prefix('category')->group(function () {
+//     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+//     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+//     Route::get('/home-care', [ProductController::class, 'homeCare']);
+//     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+// });
