de2147d4214d5683d909bccca256a6bf70230888
SuryaRf <ayruzrahmad@gmail.com>
2025-03-04 13:53:53 +0700

    user and model controller

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user'; // Mendefinisikan nama tabel
+    protected $primaryKey = 'user_id'; // Mendefinisikan primary key
 
-    // Mendefinisikan nama tabel yang digunakan oleh model ini
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'name', 'password'];
 }
