2999195576ba6188279fedc56003bd8f0e5b06e6
SuryaRf <ayruzrahmad@gmail.com>
2025-04-15 16:00:01 +0700

    Multi-Level Authorization

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -24,8 +24,39 @@ class UserModel extends Model
     protected $hidden = ['password'];
     protected $cast = ['password' => 'hashed'];
 
+    /**
+     * Relasi ke tabel level
+     */
     public function level(): BelongsTo
-{
-return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-}
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    /**
+     * Mendapatkan nama role
+     */
+   /**
+     * Mendapatkan nama role
+     */
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    /**
+     * Cek apakah user memiliki role tertentu
+     */
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
+
+    /**
+     * Mendapatkan kode role
+     */
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
+
 }
